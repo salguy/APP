8cc7a7b5a90ba4a9f6fe29f2acfb36b2502bbd2b
kth0910 <kth_04@naver.com>
2025-03-20 18:23:49 +0900

    ㅎ

--- main.py
+++ main.py
@@ -31,3 +31,8 @@ app.add_middleware(
 
 
 app.include_router(medicineInfoRouter.router, tags=["medicineInfo"])
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
