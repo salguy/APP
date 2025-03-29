f9a62d19bd51043e507da1c40c3b44ab8d52292b
kth0910 <kth_04@naver.com>
2025-03-30 04:09:53 +0900

    약 추가 엔드포인트 작성

--- domain/medications/crud.py
+++ domain/medications/crud.py
@@ -10,3 +10,12 @@ def get_medications_all(db: Session):
         raise ValueError(f"등록된 약을 찾을 수 없습니다.")
 
     return {"medication_list": medication_list}
+
+def add_pills(db:Session, record: MedicationAdd):
+    medication = Medication(
+        name = record.medication_name
+    )
+    db.add(medication)
+    db.commit()
+    db.refresh(medication)
+    return f"medication 추가 완료! id: {medication.id}, name: {medication.name}"
--- domain/medications/router.py
+++ domain/medications/router.py
@@ -15,3 +15,15 @@ async def get_medication_names(db: Session = Depends(get_db)):
         return get_medications_all(db)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+    
+    
+@router.post("/api/medications", summary="약 정보 추가")
+async def add_medications(record: MedicationAdd, db: Session = Depends(get_db)):
+    """
+    약의 정보를 추가하는 엔드포인트입니다.
+    **pill_name** : str
+    """
+    try:
+        return add_pills(db, record)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
