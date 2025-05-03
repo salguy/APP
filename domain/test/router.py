02acc5caa86cef4186fc8195e0d01d8d9bc10487
kth0910 <kth_04@naver.com>
2025-05-03 16:56:35 +0900

    queue 로그 추가

--- domain/test/router.py
+++ domain/test/router.py
@@ -17,6 +17,7 @@ async def send_message(user_id: str, message: str):
     if user_id not in queues:
         raise HTTPException(status_code=404, detail="User not found in queues")
     await queues[user_id].put(message)
+    print(queues)
     await asyncio.sleep(0)  # 컨텍스트 스위칭 강제
 
     
